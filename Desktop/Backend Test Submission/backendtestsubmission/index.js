ccec79bf17facc60a5b062bc58447317794c5bbf
04likhitha <medavarapulikhitha04@gmail.com>
2025-07-28 12:54:12 +0530

    Initial commit - Express.js app

---
+++ Desktop/Backend Test Submission/backendtestsubmission/index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+// app.post("/shorturls", (req, res) => {
+//   const { url, validity, shortcode } = req.body;
+//   const current = new Date();
+//   const expiry = new Date(current.getTime() + validity * 60 * 1000);
+//   const expiryformat = expiry.toISOString();
+//   res.json({
+//     shortLink: `https://localhost:3000/${shortcode}`,
+//     expiry: expiryformat,
+//   });
+// });
+
+const sLinks = {};
+app.post("/shorturls", (req, res) => {
+  const { url, validity, shortcode } = req.body;
+
+  const current = new Date();
+  const expiry = new Date(current.getTime() + validity * 60 * 1000);
+
+  sLinks[shortcode] = {
+    url,
+    createdAt: current.toISOString(),
+    expiry: expiry.toISOString(),
+    clicks: 0,
+    cDetails: [],
+  };
+
+  res.json({
+    shortLink: `http://localhost:${port}/${shortcode}`,
+    expiry: expiry.toISOString(),
+  });
+});
+
+app.get("/:shortcode", (req, res) => {
+  const { shortcode } = req.params;
+  const data = sLinks[shortcode];
+
+  const now = new Date();
+  if (new Date(data.expiry) < now) {
+    return res.status(410).json({ error: "Short link expired" });
+  }
+
+  data.clicks++;
+  data.cDetails.push({
+    timestamp: now.toISOString(),
+    source: req.get("User-Agent"),
+    location: "India",
+  });
+
+  res.redirect(data.url);
+});
+
+app.get("/shorturls/:shortcode", (req, res) => {
+  const { shortcode } = req.params;
+  const data = sLinks[shortcode];
+
+  res.json({
+    totalClicks: data.clicks,
+    url: data.url,
+    createdAt: data.createdAt,
+    expiry: data.expiry,
+    clicks: data.cDetails,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
